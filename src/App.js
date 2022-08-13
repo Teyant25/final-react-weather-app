75d7e6083456a427421f498d64e9e492c8433d3d
Teyant25 <96316475+Teyant25@users.noreply.github.com>
2022-08-13 09:55:39 -0400

    added bootstrap and created weather component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tya Kent and is open-sourced on {""}{" "}
-        <a
-          href="https://github.com/Teyant25/final-react-weather-app"
-          target="_blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Tya Kent and is open-sourced on {""}{" "}
+          <a
+            href="https://github.com/Teyant25/final-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
