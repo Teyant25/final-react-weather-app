69a9f0d156a5dd236c2682519646c3f589bce763
Teyant25 <96316475+Teyant25@users.noreply.github.com>
2022-08-13 22:22:10 -0400

    added form/search info

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="Submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1> New York</h1>
       <ul>
         <li> Saturday 09:00 PM</li>
@@ -13,7 +27,7 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Partly Cloudy"
           />
-          °F
+          74°F
           <div className="col-6">
             <ul>
               <li>Precipitation: 15%</li>
