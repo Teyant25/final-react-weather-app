0f9f911ac894ea492e79ce89b9f0ce61c3a4df29
Teyant25 <96316475+Teyant25@users.noreply.github.com>
2022-08-13 22:12:21 -0400

    created  weather component css file, created app layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Weather from "./Weather";
+import "./Weather.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
         <footer>
           This project was coded by Tya Kent and is open-sourced on {""}{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,1 +1,28 @@
-export default function Weather() {}
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1> New York</h1>
+      <ul>
+        <li> Saturday 09:00 PM</li>
+        <li> Partly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly Cloudy"
+          />
+          °F
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
